d44384a66be6780548411b278dd9456f5548b163
k1m190r <148415+k1m190r@users.noreply.github.com>
2023-12-12 23:38:04 +0100

    runPrompt()

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -1,6 +1,8 @@
 package com.craftinginterpreters.lox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,11 +24,21 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
     }
 
-    private static void run(String code){
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader((System.in));
+        BufferedReader reader = new BufferedReader((input));
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+        }
 
     }
 
-    private static void runPrompt() throws IOException{
-
+    private static void run(String code) throws IOException {
     }
+
 }
