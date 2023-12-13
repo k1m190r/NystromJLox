25d3debfbba5ed8dde644589b6c37e6acc96e166
k1m190r <148415+k1m190r@users.noreply.github.com>
2023-12-13 20:30:17 +0100

    update

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -66,7 +66,6 @@ public class Scanner {
             case '*':
                 addToken(STAR);
                 break;
-
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
