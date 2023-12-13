0551d292ae7e008a278157a9b216cf10ccbb2362
k1m190r <148415+k1m190r@users.noreply.github.com>
2023-12-13 01:04:02 +0100

    scanner.java update

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -64,20 +64,92 @@ public class Scanner {
             case '*':
                 addToken(STAR);
                 break;
+
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace.
+                break;
+
+            case '\n':
+                line++;
+                break;
+            case '"':
+                string();
+                break;
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
         }
     }
 
-    private char advance(){
+    private char advance() {
         return source.charAt(current++);
     }
 
-    private void addToken(TokenType type){
+    private void addToken(TokenType type) {
         addToken(type, null);
     }
 
-    private void addToken(TokenType type, Object literal){
+    private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
 
+    private boolean match(char expected) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd())
+            return '\0';
+        return source.charAt(current);
+    }
+    
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+          if (peek() == '\n') line++;
+          advance();
+        }
+    
+        if (isAtEnd()) {
+          Lox.error(line, "Unterminated string.");
+          return;
+        }
+    
+        // The closing ".
+        advance();
+    
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+      }
+    
+    
 }
